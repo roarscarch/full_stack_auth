ab0916662d31378bdb2be6a4694ddc5153bfddad
roarscarch <2020ugee082@nitjsr.ac.in>
2023-10-08 17:52:52 +0530

    header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,14 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Home from './pages/Home';
+import Header from './components/Header';
 
 export default function App() {
   return (
+   
     <BrowserRouter>
+     <Header/>
+    
     <Routes>
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <div className="bg-sky-200 flex items-center gap-7 mx-auto">
+        <Link to= "/">
+        <div className="font-bold">Auth App</div>
+        </Link>
+        <ul className="flex gap-4">
+            <Link to="/profile"><li> Profile</li></Link>
+            
+            <Link to="/sign-in"><li> SignIn</li></Link>
+            <Link to="/about"><li> About</li></Link>
+        </ul>
+        
+    </div>
+  )
+}
