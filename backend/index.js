80555950e51c276044b1a4b0eaffd89264c86224
roarscarch <2020ugee082@nitjsr.ac.in>
2023-10-09 01:12:54 +0530

    made new db and conncted it

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,34 @@
 import express  from "express";
 const app = express();
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();// initialising dotenv configuration
+
+
+
+mongoose
+.connect(process.env.DB_URL)
+.then(()=>{
+    console.log("Connected to MongoDB")
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000,()=>{
     console.log("app listening on port 3000");
-})
+});
